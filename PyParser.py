501d5c7d78da523edf73d5364d84bb3c4d8cbc84
Blakletter <48567475+Blakletter@users.noreply.github.com>
2019-09-27 23:33:17 -0700

    Update PyParser.py

--- PyParser.py
+++ PyParser.py
@@ -7,11 +7,11 @@ import math
 
 class PyParser:
     def __init__(self):
-        self.pos = 0
-        self.ch = ''
-        self.string = ''
-        self.variables = {}
-        self.help = """Complete list of functions includes:
+        self.__pos = 0
+        self.__ch = ''
+        self.__string = ''
+        self.__variables = {}
+        self.__help = """Complete list of functions includes:
 +
 -
 *
@@ -37,18 +37,18 @@ help
 """
     #Sets ch to the current character and moves the 'cursor' down one
     def nextChar(self):
-        if self.pos < len(self.string):
-            self.ch = self.string[self.pos]
-            self.pos+=1
+        if self.__pos < len(self.__string):
+            self.__ch = self.__string[self.__pos]
+            self.__pos+=1
 
         else:
-            self.ch = ''
+            self.__ch = ''
     #Checks to see if ch == +, -, *, /, etc.
     #If it is, shifts cursor to right and returns true
     def eat(self, charToEat):
-        while self.ch == ' ':
+        while self.__ch == ' ':
             self.nextChar()
-        if self.ch == charToEat:
+        if self.__ch == charToEat:
             self.nextChar()
             return True
         return False
@@ -56,8 +56,8 @@ help
     #Parse term keeps shifting cursor to the right until it hits non-float
     #Returns float
     def parseTerm(self):
-        #global self.pos
-        self.pos_ = self.pos
+        #global self.__pos
+        self.pos_ = self.__pos
         #First check for unary + or - to handle double -'s'
         if self.eat('-'):
             x = -1*self.parseTerm()
@@ -68,15 +68,15 @@ help
             x = self.parseExpression()
             self.eat(')')
         #If its a digit, run cursor to the right
-        elif self.ch.isdigit() or self.ch == '.':
-            while self.ch.isdigit() or self.ch == '.':
+        elif self.__ch.isdigit() or self.__ch == '.':
+            while self.__ch.isdigit() or self.__ch == '.':
                 self.nextChar()
-            x = self.string[self.pos_-1:self.pos-1]
+            x = self.__string[self.pos_-1:self.__pos-1]
         else:
             #Gather function name here
-            while not self.ch.isdigit() and (self.ch.isalpha()):
+            while not self.__ch.isdigit() and (self.__ch.isalpha()):
                 self.nextChar()
-            x = self.string[self.pos_-1:self.pos-1]
+            x = self.__string[self.pos_-1:self.__pos-1]
 
             #Define functions here
             if x == 'sqrt':
@@ -106,7 +106,7 @@ help
             elif x == 'fact':
                 x = math.factorial(self.parseTerm())
             elif x == 'clear':
-                    self.variables.clear()
+                    self.__variables.clear()
                     return "Done"
             elif x == 'pi':
                 x = math.pi
@@ -114,8 +114,8 @@ help
                 x = math.e
             elif x == 'help':
                 return self.help
-            if x in self.variables:
-                return self.variables.get(x, 0)
+            if x in self.__variables:
+                return self.__variables.get(x, 0)
             else:
                 #Error breaking, wasn't a function float or parenthese
                 return x + "\ntype 'help' for list of functions"
@@ -155,7 +155,7 @@ help
         while True:
             if self.eat('='):
                 if not any(char.isdigit() for char in str(x)):
-                    self.variables[x] = self.parseExpression()
+                    self.__variables[x] = self.parseExpression()
                     return "Variable "+x+" set."
                 else:
                     if x == self.parseExpression():
@@ -182,9 +182,9 @@ help
                 x = str(x) + ", " + str(self.parseEquals())
             else:
                 return x
-    def eval(self, str_):
-        self.pos = 0
-        self.string = str_+" "
+    def parse(self, str_):
+        self.__pos = 0
+        self.__string = str_+" "
         self.nextChar()
         try:
             x = self.parseProblem()
@@ -192,7 +192,7 @@ help
             return e
         return x
     def setVariables(self, l):
-        self.variables = dict(self.variables, **l)
+        self.__variables = dict(self.__variables, **l)
         return True
     def clearVariables(self):
-        self.variables.clear()
+        self.__variables.clear()
